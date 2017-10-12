d536fb1ab811afb77a694faefa543aa4ed669f10
Sascha Depold <sascha@depold.com>
2017-10-12 21:13:47 +0200

    Add site navigation

---
+++ src/components/site-navigation/helper.js
@@ -0,0 +1,13 @@
+const helper = module.exports = {
+  extractHeadlines: (html) => {
+    return html.match(/<h(\d).*?>(.*?)<\/h\d>/g).map((header) => {
+      const match = header.match(/<h(\d)(.*?)>(.*?)<\/h\d>/)
+      return { level: match[1], ref: helper.extractId(match[2]), text: match[3] };
+    });
+  },
+
+  extractId: (ref) => {
+    const match = ref.match(/id="(.*)"/);
+    return match ? `#${match[1]}` : null;
+  }
+};
