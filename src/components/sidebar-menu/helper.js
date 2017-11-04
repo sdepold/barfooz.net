1283d24637668c13db2f50b49ca5108478a04f13
Sascha Depold <sascha@depold.com>
2017-11-04 23:02:08 +0100

    Restructure page

--- src/components/sidebar-menu/helper.js
+++ src/components/sidebar-menu/helper.js
@@ -1,6 +1,6 @@
 const helper = module.exports = {
   extractHeadlines: (html) => {
-    return html.match(/<h(\d).*?>(.*?)<\/h\d>/g).map((header) => {
+    return (html.match(/<h(\d).*?>(.*?)<\/h\d>/g) || []).map((header) => {
       const match = header.match(/<h(\d)(.*?)>(.*?)<\/h\d>/)
       return { level: match[1], ref: helper.extractId(match[2]), text: match[3] };
     });
